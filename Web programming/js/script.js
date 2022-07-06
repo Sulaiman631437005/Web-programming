88ee812a3437f34cdff65471486bb94a9673601a
Sulaiman631437005 <96813241+Sulaiman631437005@users.noreply.github.com>
2022-07-06 09:59:48 +0700

    Add files via upload

---
+++ Web programming/js/script.js
@@ -0,0 +1,14 @@
+var type = new Typed(".hello",{
+    strings:["Sulaiman Charaae"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+var type = new Typed(".fatoni",{
+    strings:["Fatoni University"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
